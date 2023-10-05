dde562c872b61b4e639fa281c0f5c48cd7d8a4cc
sukrusimsek <sukrusimsekll@gmail.com>
2023-10-05 21:11:31 +0300

    new commit
    I added images for campaign page. I added collectionview for campaign page.

--- PazaramaApp/TabController/CampaignController.swift
+++ PazaramaApp/TabController/CampaignController.swift
@@ -8,23 +8,95 @@
 import UIKit
 
 class CampaignController: UIViewController {
-
+    //MARK: - Properties
+    let images = ["1","2","3","4","5","6","7"]
+    
+    var collectionView: UICollectionView!
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
-        super.viewDidLoad()
+        //super.viewDidLoad()
         view.backgroundColor = .white
+       
+        style()
+        layout2()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        
+    }
+}
 
-        // Do any additional setup after loading the view.
+    //MARK: - Helpers
+extension CampaignController {
+    
+    
+    private func style() {
+        
+        
+        
+    }
+    private func layout2() {
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+
+        ])
+    }
+}
+extension CampaignController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 20, height: view.frame.height/5)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension CampaignController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.imageImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Detay Sayfasına Gidilecek \(indexPath.row)")
+    }
+    
+    
+}
+class CustomCell: UICollectionViewCell {
+    let imageImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageImageView)
+        imageImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageImageView.layer.cornerRadius = 10
+        imageImageView.layer.masksToBounds = true
+        NSLayoutConstraint.activate([
+            imageImageView.topAnchor.constraint(equalTo: topAnchor),
+            imageImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
